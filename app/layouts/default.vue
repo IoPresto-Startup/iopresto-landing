1f49cf69cee82daf7b795823a5f2ad1aa4dcefdf
LucaRanieri96 <luca.ranieri.4@gmail.com>
2025-10-01 22:29:53 +0200

    minor fix

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,8 +6,9 @@ import ScrollToTop from '~/components/generic/ScrollToTop.vue';
 <template>
     <LayoutHeader />
     <UMain class="relative min-h-screen overflow-x-hidden">
-        <div class="absolute top-0 left-0 w-full h-full -z-10 bg-cover bg-center bg-no-repeat"
+        <div class="fixed top-0 left-0 w-full h-screen -z-10 bg-cover bg-center bg-no-repeat"
             style="background-image: url('/img/bg/pavoni_bg.jpg');"></div>
+
         <UContainer>
             <slot />
         </UContainer>
