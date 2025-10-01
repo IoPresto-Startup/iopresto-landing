407cb92527af0b2603d2d1bfdf3ab9e544f55c49
Claudio Fanzini <claudio.fanzini@aidia.it>
2025-10-01 16:59:56 +0200

    updated header, favicon, components syntax

--- app/components/about-us/DialogDescription.vue
+++ app/components/about-us/DialogDescription.vue
@@ -11,13 +11,12 @@
 		<template #title>
 			<p class="text-4xl font-bold text-orange-500">Chi siamo</p>
 		</template>
-		<!-- <template #body>
-			<p>{{ $t('who.total-description', { br: () => "<br />" }) }}</p>
-		</template> -->
+		<template #body>
+			<p>{{ $t("about-us.small-description") }}</p>
+		</template>
 	</UModal>
 </template>
 
 <script setup lang="ts">
 const openDialog = defineModel<boolean>("openDialog")
-const { t } = useI18n()
 </script>
--- app/components/layout/header.vue
+++ app/components/layout/header.vue
@@ -1,32 +1,32 @@
 <template>
-    <UHeader>
-        <template #title>
-            IOPRESTO
-        </template>
+	<UHeader>
+		<template #title>
+			<NuxtImg src="/img/iopresto_logo_2.png" class="w-20 h-10" />
+		</template>
 
-        <UNavigationMenu :items="items" />
+		<UNavigationMenu :items="items" />
 
-        <template #right>
-            <UColorModeButton />
-        </template>
-    </UHeader>
+		<template #right>
+			<UColorModeButton />
+		</template>
+	</UHeader>
 </template>
 
 <script setup lang="ts">
-import type { NavigationMenuItem } from '@nuxt/ui';
+import type { NavigationMenuItem } from "@nuxt/ui"
 
 const route = useRoute()
 
 const items = computed<NavigationMenuItem[]>(() => [
-    {
-        label: 'Home',
-        to: '/',
-        active: route.path === '/'
-    },
-    {
-        label: 'Contatti',
-        to: '/contatti',
-        active: route.path === '/contatti'
-    }
+	{
+		label: "Home",
+		to: "/",
+		active: route.path === "/"
+	},
+	{
+		label: "Contatti",
+		to: "/contatti",
+		active: route.path === "/contatti"
+	}
 ])
 </script>
