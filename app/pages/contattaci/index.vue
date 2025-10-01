895aca565d9566e32f2ad30dad72726771b52e5e
LucaRanieri96 <luca.ranieri.4@gmail.com>
2025-10-01 21:55:53 +0200

    added contact form

---
+++ app/pages/contattaci/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="py-8 px-4">
+        <ContactForm />
+    </div>
+</template>
+
+<script setup lang="ts">
+import ContactForm from '~/components/contact/contactForm.vue'
+</script>
